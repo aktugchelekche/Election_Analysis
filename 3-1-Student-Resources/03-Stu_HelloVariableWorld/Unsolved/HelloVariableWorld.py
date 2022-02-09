49bee0c377b26175e0e7f3f303806a8842d7dcbc
AktugChelekche <aktugcilekci@hotmail.com>
2022-02-08 20:05:02 -0600

    Creating Variables

--- 3-1-Student-Resources/03-Stu_HelloVariableWorld/Unsolved/HelloVariableWorld.py
+++ 3-1-Student-Resources/03-Stu_HelloVariableWorld/Unsolved/HelloVariableWorld.py
@@ -1,33 +1,37 @@
 # Create a variable called 'name' that holds a string
-
+name = "Aktug"
 
 # Create a variable called 'country' that holds a string
-
+country = "USA"
 
 # Create a variable called 'age' that holds an integer
-
+age = "Young"
 
 # Create a variable called 'hourly_wage' that holds an integer
 
-
+hourly_wage = 59.45
 # Calculate the daily wage for the user
-
+daily_wage = hourly_wage*8- (hourly_wage*8)
 
 # Create a variable called 'satisfied' that holds a boolean
-
+satisfied = True
 
 # Print out "Hello <name>!"
 
+print("Hello my name is  " + name)
 
 # Print out what country the user entered
 
+print("I am entering from " + country)
 
 # Print out the user's age
 
+print(str(age))
 
 # With an f-string, print out the daily wage that was calculated
 
+print(f"My daily wage is : {daily_wage}")
 
 # With an f-string, print out whether the users were satisfied
 
-print("Hr")
+print(f"I am  : {satisfied}")
