0b5765fb2e094e7ae6c5d4b39918c5585dade5aa
AktugChelekche <aktugcilekci@hotmail.com>
2022-02-10 14:02:56 -0600

    PRS Game

--- 3-2-Student-Resources/03-Par_RockPaperScissors/Unsolved/RPS_Unsolved.py
+++ 3-2-Student-Resources/03-Par_RockPaperScissors/Unsolved/RPS_Unsolved.py
@@ -5,22 +5,38 @@ import random
 print("Let's Play Rock Paper Scissors!")
 
 # Specify the three options
-options = ["r", "p", "s"]
+options = ["Rock", "Paper", "Scissors"]
 
 # Computer Selection
 computer_choice = random.choice(options)
 
 # User Selection
-user_choice = input("Make your Choice: (r)ock, (p)aper, (s)cissors? ")
+user_choice = input("Make your Choice: Rock, Paper, Scissors? ")
 
 # Run Conditionals
-
 if computer_choice == user_choice : 
-     print("Try Again!")
+     print("Game Tie")
 
-if computer_choice == 'r ' and user_choice == ' s ' :
-    print("You lost")
-if computer_choice == 's ' and user_choice == ' r ' :
-    print("You win")
-if computer_choice == 'p ' and user_choice == ' s ' :
-    print("You win")
+
+elif user_choice == "Paper" :
+    if computer_choice == "Scissors" :
+        print(f"You lose , {computer_choice} cut {user_choice} ")
+    else : 
+         print(f"You win, {user_choice} covers {computer_choice}")
+elif user_choice == "Scissors" :
+    if computer_choice == "Rock" :
+        print(f"You lose , {computer_choice} smashes {user_choice} ")
+    else :
+        print(f"You win, {user_choice} cuts {computer_choice}")
+elif user_choice == "Rock":  
+    if computer_choice == "Paper":
+        print(f"You lose , {computer_choice} covers {user_choice} ")
+    else :
+        print(f"You win, {user_choice} smashes {computer_choice} ")
+
+
+
+
+
+     
+
