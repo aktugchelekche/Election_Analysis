4c7f26027a0edb365852a1180e8b207dbe47c176
AktugChelekche <aktugcilekci@hotmail.com>
2022-02-11 09:28:02 -0600

    Chain Updated

--- 3-2-Student-Resources/03-Par_RockPaperScissors/Unsolved/RPS_Unsolved.py
+++ 3-2-Student-Resources/03-Par_RockPaperScissors/Unsolved/RPS_Unsolved.py
@@ -1,38 +1,43 @@
 # Incorporate the random library
 import random
+while True:
+    # Print Title
+    print("Let's Play Rock Paper Scissors!")
 
-# Print Title
-print("Let's Play Rock Paper Scissors!")
+    # Specify the three options
+    options = ["Rock", "Paper", "Scissors"]
 
-# Specify the three options
-options = ["Rock", "Paper", "Scissors"]
+    # Computer Selection
+    computer_choice = random.choice(options)
 
-# Computer Selection
-computer_choice = random.choice(options)
+    # User Selection
 
-# User Selection
-user_choice = input("Make your Choice: Rock, Paper, Scissors? ")
-
-# Run Conditionals
-if computer_choice == user_choice : 
-     print("Game Tie")
+    user_choice = input("Make your Choice: Rock, Paper, Scissors? ")
 
 
-elif user_choice == "Paper" :
-    if computer_choice == "Scissors" :
-        print(f"You lose , {computer_choice} cut {user_choice} ")
-    else : 
-         print(f"You win, {user_choice} covers {computer_choice}")
-elif user_choice == "Scissors" :
-    if computer_choice == "Rock" :
-        print(f"You lose , {computer_choice} smashes {user_choice} ")
-    else :
-        print(f"You win, {user_choice} cuts {computer_choice}")
-elif user_choice == "Rock":  
-    if computer_choice == "Paper":
-        print(f"You lose , {computer_choice} covers {user_choice} ")
-    else :
-        print(f"You win, {user_choice} smashes {computer_choice} ")
+    # Run Conditionals
+    if computer_choice == user_choice : 
+        print("Game Tie")
+    elif user_choice == "Rock":  
+        if computer_choice == "Paper":
+            print(f"You lose , {computer_choice} covers {user_choice} ")
+        else :
+            print(f"You win, {user_choice} smashes {computer_choice} ")
+
+    elif user_choice == "Paper" :
+        if computer_choice == "Scissors" :
+            print(f"You lose , {computer_choice} cut {user_choice} ")
+        else : 
+            print(f"You win, {user_choice} covers {computer_choice}")
+    elif user_choice == "Scissors" :
+        if computer_choice == "Rock" :
+            print(f"You lose , {computer_choice} smashes {user_choice} ")
+        else :
+            print(f"You win, {user_choice} cuts {computer_choice}")
+    
+    play_again = input("Play Again ? (y/n ) :")
+    if play_again != "y" :
+        break
 
 
 
